ac2e6f034fff86448b3818d0bc24611b02b1696f
Jason R. Carrete <jasoncarrete5@gmail.com>
2019-06-09 17:37:05 -0400

    Implement prediction

--- recommender/calc.py
+++ recommender/calc.py
@@ -1,19 +1,40 @@
 import sqlite3
 
+# All restaurants that a teammate likes
 TEAM_LIKES_SQL = """
     SELECT restaurantId
     FROM ratings
     WHERE rating='LIKE' AND teammateId=?
 """
+# All restaurants that a teammate dislikes
 TEAM_DISLIKES_SQL = """
     SELECT restaurantId
     FROM ratings
     WHERE rating='DISLIKE' AND teammateId=?
 """
+# Count of restaurants that either of two teammates have rated
 TOTAL_SQL = """
     SELECT COUNT(DISTINCT restaurantId)
     FROM ratings
     WHERE teammateId=? OR teammateId=?
+"""
+# All teammates that like a restaurant
+RES_LIKES_SQL = """
+    SELECT teammateId
+    FROM ratings
+    WHERE rating='LIKE' AND restaurantId=:r AND teammateId!=:t
+"""
+# All teammates that dislike a restaurant
+RES_DISLIKES_SQL = """
+    SELECT teammateId
+    FROM ratings
+    WHERE rating='DISLIKE' AND restaurantId=:r AND teammateId!=:t
+"""
+# Count of teammates that have rated a restaurant
+RES_TOTAL_SQL = """
+    SELECT COUNT(DISTINCT teammateId)
+    FROM ratings
+    WHERE restaurantId=:r
 """
 
 
@@ -34,5 +55,11 @@ def similarity(db_conn: sqlite3.Connection, t1_id: str, t2_id: str) -> float:
     return (both_like + both_dislike - like_dislike - dislike_like) / total
 
 
-def prediction(t_id: str, r_id: str) -> float:
-    pass
+def prediction(db_conn: sqlite3.Connection, t_id: str, r_id: str) -> float:
+    args = {'t': t_id, 'r': r_id}
+    other_liked = set(t[0] for t in db_conn.execute(RES_LIKES_SQL, args))
+    like_sum = sum(similarity(db_conn, t_id, other) for other in other_liked)
+    other_disliked = set(t[0] for t in db_conn.execute(RES_DISLIKES_SQL, args))
+    dislike_sum = sum(similarity(db_conn, t_id, other) for other in other_disliked)
+    total = db_conn.execute(RES_TOTAL_SQL, args).fetchone()[0]
+    return (like_sum - dislike_sum) / total
--- tests/calc_test.py
+++ tests/calc_test.py
@@ -1,5 +1,6 @@
 import sqlite3
 from importlib.resources import open_text
+from math import isclose
 
 import pytest
 
@@ -107,3 +108,68 @@ def test_similarity_index(db_conn: sqlite3.Connection):
                            "e17b91cb-5a2c-4055-befb-1d1ea9f7daca",
                            "e17b91cb-5a2c-4055-befb-1d1ea9f7daca")
     assert test == 1.0
+
+
+def test_res_likes_sql(db_conn: sqlite3.Connection):
+    args = {
+        'r': "jS3affmkXWrBnidXA-DIfQ",
+        't': "e17b91cb-5a2c-4055-befb-1d1ea9f7daca"
+    }
+    test = set(t[0] for t in db_conn.execute(calc.RES_LIKES_SQL, args))
+    actual = {
+        "67004926-1b19-427e-a764-de8c917f3d15"
+    }
+    assert test == actual
+    args = {
+        'r': "jS3affmkXWrBnidXA-DIfQ",
+        't': "3714b577-b693-4cb0-a77c-a6616c692225"
+    }
+    test = set(t[0] for t in db_conn.execute(calc.RES_LIKES_SQL, args))
+    actual = {
+        "67004926-1b19-427e-a764-de8c917f3d15",
+        "e17b91cb-5a2c-4055-befb-1d1ea9f7daca"
+    }
+    assert test == actual
+
+
+def test_res_dislikes_sql(db_conn: sqlite3.Connection):
+    args = {
+        'r': "jS3affmkXWrBnidXA-DIfQ",
+        't': "e17b91cb-5a2c-4055-befb-1d1ea9f7daca"
+    }
+    test = set(t[0] for t in db_conn.execute(calc.RES_DISLIKES_SQL, args))
+    actual = {
+        "3714b577-b693-4cb0-a77c-a6616c692225"
+    }
+    assert test == actual
+    args = {
+        'r': "6ajnOk0GcY9xbb5Ocaw8Gw",
+        't': "e17b91cb-5a2c-4055-befb-1d1ea9f7daca"
+    }
+    test = set(t[0] for t in db_conn.execute(calc.RES_DISLIKES_SQL, args))
+    actual = {
+        "67004926-1b19-427e-a764-de8c917f3d15",
+        "3714b577-b693-4cb0-a77c-a6616c692225"
+    }
+    assert test == actual
+
+
+def test_res_total_sql(db_conn: sqlite3.Connection):
+    args = {'r': "6ajnOk0GcY9xbb5Ocaw8Gw"}
+    test = db_conn.execute(calc.RES_TOTAL_SQL, args).fetchone()[0]
+    assert test == 3
+    args = {'r': "jS3affmkXWrBnidXA-DIfQ"}
+    test = db_conn.execute(calc.RES_TOTAL_SQL, args).fetchone()[0]
+    assert test == 3
+    args = {'r': "mtvT7uRey3F395STFRM1Tg"}
+    test = db_conn.execute(calc.RES_TOTAL_SQL, args).fetchone()[0]
+    assert test == 2
+
+
+def test_prediction(db_conn: sqlite3.Connection):
+    args = {
+        'r_id': "6ajnOk0GcY9xbb5Ocaw8Gw",
+        't_id': "e17b91cb-5a2c-4055-befb-1d1ea9f7daca"
+    }
+    test = calc.prediction(db_conn, **args)
+    assert isclose(test, -0.0833, rel_tol=1e-3)
